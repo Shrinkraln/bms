dd0a6edd0b549031197e1304899b7b167cc6de1c
Shrinkraln <2133275265@qq.com>
2026-05-09 06:00:01 +0800

    pcd_v2.0

--- bms_proj/Core/Inc/bms_protect.h
+++
@@ -1,8 +0,0 @@
-#ifndef BMS_PROTECT_H
-#define BMS_PROTECT_H
-
-#include "bms.h"
-
-void BMS_Protect_Check(BMS_Handle *bms);
-
-#endif /* BMS_PROTECT_H */
--- bms_proj/MDK-ARM/bms_proj/startup_stm32g474xx.d
+++
@@ -1,1 +0,0 @@
-bms_proj\startup_stm32g474xx.o: startup_stm32g474xx.s
